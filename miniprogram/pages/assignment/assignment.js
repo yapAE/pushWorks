535d24b3b0ad43d1532f720ca66a2f80031701eb
Terry <lihao@bidangjia.io>
2019-07-29 20:48:12 +0800

    pushWork

--- miniprogram/pages/assignment/assignment.js
+++ miniprogram/pages/assignment/assignment.js
@@ -1,20 +1,31 @@
 // pages/assignment/assignment.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    textareaValue: '',
+    content: '',
+    title: '',
     index: null,
     imgList: [],
     files: [],
+    parentId: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options.pid)
+    if(options.pid){
+      this.setData({
+        parentId: options.pid
+      })
+    }
+
 
   },
 
@@ -108,9 +119,9 @@ Page({
       }
     })
   },
-  textareaInput(e) {
+  bindInputContent(e) {
     this.setData({
-      textareaValue: e.detail.value
+      content: e.detail.value
     })
   },
 
@@ -120,27 +131,139 @@ Page({
       title: '提交中...',
     })
   //上传图片
-    for (let image in this.data.imgList) {
-      let filePath = this.data.imgList[image]
-      let siffix = /\.[^\.]+$/.exec(filePath)[0];//获取文件扩展名
-      var now = new Date().getTime()
-      wx.cloud.uploadFile({
-        cloudPath: now + siffix,
-        filePath: filePath
-      }).then(res => {
-        console.log(res.fileID)
-        this.data.files = this.data.files.concat(res.fileID)
+  const  PromiseArr = []
+
+    PromiseArr.push(this.uploadImg())
+    Promise.all(PromiseArr).then(res =>{
+      this.pushData().then(re => {
+        this.setData({
+          imgList: [],
+          title: '',
+          content: ''
+        })
+        wx.hideLoading()
+        wx.showToast({
+          title: '作业布置完毕',
+        })
       })
-    }
+    })
 
     console.log(this.data.files)
+  },
+  //图片上传
+  uploadImg(){
+    for (let image in this.data.imgList) {
+        let filePath = this.data.imgList[image]
+        let siffix = /\.[^\.]+$/.exec(filePath)[0];//获取文件扩展名
+      new Promise((resolve, reject) => {
+        var now = new Date().getTime()
+        wx.cloud.uploadFile({
+          cloudPath: now + siffix,
+          filePath: filePath
+        }).then(res => {
+       //   console.log(res.fileID)
+          this.data.files = this.data.files.concat(res.fileID)
+          console.log(this.data.files)
+          resolve()
+        }).catch(error=>{
+          console.log(error)
+        })
+      })
+    }
+  },
 
+  //数据入库
+  pushData(){
+   return new Promise((resolve,reject)=>{
+      db.collection('works').add({
+        data: {
+          avatarUrl: app.globalData.avatarUrl,
+          title: this.data.title,
+          content: this.data.content,
+          images: this.data.files,
+          parentId: this.data.parentId,
+          time: new Date().getTime()
+        }
+      }).then(res =>{
+       // console.log(res.data)
+        resolve()
+      }).catch(error =>{
+        console.log(error)
+      })
+    })
 
-    wx.showToast({
-      title: '作业布置完毕',
+  },
+  //绑定标题数据
+  bindInputTitle(e) {
+    this.setData({
+      title: e.detail.value
     })
   },
 
-  //入库
+  submitWork: function () {
+    wx.showLoading({
+      title: '提交中',
+    })
+    //定义一个上传数组
+    const promiseArr = []
+
+    for (let i = 0; i < this.data.imgList.length; i++) {
+      let filePath = this.data.imgList[i]
+      let siffix = /\.[^\.]+$/.exec(filePath)[0];//获取文件扩展名
+
+      promiseArr.push(new Promise((reslove, reject) => {
+
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + siffix,
+          filePath: filePath, //文件路径
+        }).then(res => {
+          //get Resource Id
+          console.log(this.data.imgList)
+          console.log(res.fileID)
+          //将返回的数据计入files[]
+          this.data.files = this.data.files.concat(res.fileID)
+          console.log(this.data.files)
+
+          reslove()
+        }).catch(error => {
+          console.log(error)
+        })
+
+      }))
+    }
+    //时间
+    var now = new Date().getTime();
+
+    Promise.all(promiseArr).then(res => {
+      console.log(this.data.images)
+      db.collection('works').add({
+        data: {
+          images: this.data.files,
+          title: this.data.title,
+          content: this.data.content,
+          time: now,
+          nickName: app.globalData.nickName,
+          avatarUrl: app.globalData.avatarUrl,
+          parentId: this.data.parentId,
+          //异步上传，结束才赋值
+        }
+      })
+        .then(res => {
+          console.log(res)
+          wx.hideLoading()
+          wx.showToast({
+            title: '提交成功',
+          })
+          this.setData({
+            imgList: [],
+            title: ''
+          })
+
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+  },
 
 })
